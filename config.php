a3f2d737d907c62dc67374a97ae6ecb90d7ecc7e
Qowface <qowface@opserve.net>
2014-01-29 14:45:22 -0500

    Modified URL pattern.

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $page['title'] = 'Factoids Web App'; // The header/title to display.
 $page['footer'] = 'Documentation and help powered by <a href="http://dev.bukkit.org/bukkit-plugins/factoids/" target="_blank">Factoids</a>'; // The footer to display.
 
 /* Don't mess with these unless you really know what you're doing */
-$util['pattern'] = array('/;{2}/', '/&+\w/', '/([a-zA-Z]+[:\/\/]+[A-Za-z0-9\-_]+\.+[A-Za-z0-9\.\/%&=\?\-_]+)/');
+$util['pattern'] = array('/;{2}/', '/&+\w/', '/([a-zA-Z]+[:\/\/]+[A-Za-z0-9\-_]+\.+[A-Za-z0-9\.\/%&=\?\-_#:]+)/');
 $util['replace'] = array('<br>', '', '<a href=$1 target="_blank">$1</a>');
 
 ?>
